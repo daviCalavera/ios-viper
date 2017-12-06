64bd11cc56bedff98ab6ec0a921c6f040e4e1e77
LuqDavid <david.nirvana@gmail.com>
2017-12-06 00:41:38 -0300

    Initial Commit

---
+++ V.I.P.E.RTests/V_I_P_E_RTests.swift
@@ -0,0 +1,36 @@
+//
+//  V_I_P_E_RTests.swift
+//  V.I.P.E.RTests
+//
+//  Created by David on 06/12/2017.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+import XCTest
+@testable import V_I_P_E_R
+
+class V_I_P_E_RTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
